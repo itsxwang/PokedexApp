a70451a525340062613558754a20c8769113a0e8
XionVolt <xion36325@gmail.com>
2025-03-01 19:43:26 +0530

    Add roll button and implement Pokémon fetching functionality; update styles for improved layout

---
+++ script.js
@@ -0,0 +1,19 @@
+const sprite = document.querySelector('.pokemon-sprite');
+
+async function getPokemon () {
+    try {
+        
+        const randomId = Math.floor(Math.random() * 1025) + 1;
+        const response = await fetch('https://pokeapi.co/api/v2/pokemon/' + randomId);
+        const pokemonData = await response.json();
+        sprite.src = pokemonData.sprites.front_default;
+        console.log(sprite)
+        console.log(pokemonData)
+}
+
+catch (error) {
+    console.log(error);
+}
+}
+;
+document.querySelector('.js-roll-btn').addEventListener('click', getPokemon);
